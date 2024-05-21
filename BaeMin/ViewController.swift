312a32b36f9474e188067e918e500455a914ce4a
lcho3878 <leech3878@naver.com>
2024-05-21 13:33:12 +0900

    Initial Commit

---
+++ BaeMin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaeMin
+//
+//  Created by 이찬호 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
